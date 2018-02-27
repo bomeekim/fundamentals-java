864a76d51c758b446034c6284a1c2c5b12160fc6
Bomi Kim <thespringg@naver.com>
2018-02-27 17:07:23 +0900

    Prob1

---
+++ assignment/Prob1.java
@@ -0,0 +1,15 @@
+package assignment;
+
+public class Prob1 {
+
+	public static void main(String[] args) {
+		/*
+		 * 아래는 변수 num의 값에따라 양수 음수 0을 출력하는 코드이다. 
+		 * 삼항 연산자를 이용해서 에 알맞은 코드를 완성하세요.
+		 * 힌트: 삼항 연산자를 두번 이용
+		 */
+		int num = -90;
+		System.out.println(num > 0 ? "양수" : (num < 0 ? "음수" : "0"));
+
+	}
+}
