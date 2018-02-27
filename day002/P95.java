a302d91343e426c78d1ec478b13e8e88a606de7f
Bomi Kim <thespringg@naver.com>
2018-02-27 14:37:14 +0900

    연산자 실습

---
+++ day002/P95.java
@@ -0,0 +1,21 @@
+package day002;
+
+public class P95 {
+
+	public static void main(String[] args) {
+		int i=5, j=5;
+		
+		// 증감 연산자
+		System.out.println(i++); // 후위형 (참조 된 후 값 증가)
+		System.out.println(++j); // 전위형 (참조 전에 값 증가)
+		
+		// 정수, 실수
+		System.out.println(1/2);
+		System.out.println(1/2.0);
+		
+		byte a=10, b=30;
+		int c = a*b;
+		System.out.println(c);
+	}
+
+}
