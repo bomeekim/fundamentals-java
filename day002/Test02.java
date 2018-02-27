1b2495093ca4a870080f8423dcf53dbae4ac5429
Bomi Kim <thespringg@naver.com>
2018-02-27 12:32:33 +0900

    Date 클래스 실습

---
+++ day002/Test02.java
@@ -0,0 +1,19 @@
+package day002;
+
+import java.util.Date;
+
+public class Test02 {
+
+	public static void main(String[] args) {
+		// "2018/03/01"
+		
+		Date today = new Date();
+		System.out.println(today);
+		
+		Date day2 = new Date("2018/03/01");
+		System.out.println(day2);
+		
+		java.sql.Date day3;
+	}
+
+}
